c300436229c6a4ebdaed3ba7fab3854189dd763d
Petch <6811850010@mut.ac.th>
2025-12-30 16:53:58 +0700

    Lab 7

---
+++ Lab7/Lab7/Lab7.cpp
@@ -0,0 +1,13 @@
+// TODO: เขียนโปรแกรมแสดงค่า x และ *p
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int* p = &x;
+
+	cout << "Value of x: " << x << endl;
+	cout << "Value of *p: " << *p << endl;
+
+    return 0;
+}
